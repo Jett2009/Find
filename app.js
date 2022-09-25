3d4b9d9ccf1641712f1d7521f91d9e48e82ce860
Braxton Jett <113302027+Jett2009@users.noreply.github.com>
2022-09-25 14:22:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 function findUserByUsername(usersArray, username) {
-	return usersArray.find(function (user) {
+	return array.find(function (user) {
 		return user.username === username;
 	});
 }
-function removeUser(usersArray, username) {
-	let foundIndex = usersArray.findIndex(function (user) {
+function removeUser(array, username) {
+	let foundIndex = array.findIndex(function (user) {
 		return user.username === username;
 	});
 	if (foundIndex === -1) return;
 
-	return usersArray.splice(foundIndex, 1)[0];
+	return array.splice(foundIndex, 1)[0];
 }
